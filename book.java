2e8ab9c3dc09c795d40f73796ef6860809157396
Karan Rana <148432637+Karanrana92@users.noreply.github.com>
2024-09-01 18:25:18 +0530

    Update book.java

--- book.java
+++ book.java
@@ -1,1 +1,46 @@
 
+// src/main/java/LibraryManagement/Book.java
+
+package LibraryManagement;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean available;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.available = true;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    // Override equals and hashCode for proper comparison in test assertions
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return isbn.hashCode();
+    }
+}
